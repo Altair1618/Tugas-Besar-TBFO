719a038c1110e89ac950461a903692416bdff0ba
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2022-11-24 16:02:37 +0700

    Add-FA-and-Fix-Bacafile

--- BacaFile.py
+++ BacaFile.py
@@ -1,40 +1,108 @@
-def bacafile(namafile):
-    # Fungsi pembacaan namafile
-    # Hasil menjadi array of string
+import os
+import re
 
-    # KAMUS
-    # f : file
 
-    # ALGORITMA
-    f = open(namafile, 'r')
+def getCurrentDirectory():
+    # Mengembalikan directory saat ini
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def bacaFile(NamaFile):
+    # Mengembalikan Array of Char Syntax dari file NamaFile
+
+    # Membaca File
+    currentDir = getCurrentDirectory()
+    f = open(os.path.join(currentDir, NamaFile), 'r')
     arr = f.readlines()
     f.close()
 
-    return arr
+    # Menghilangkan Komentar Baris (Yang menggunakan //)
+    output = ""
+    idxLine = 0
+    while idxLine < len(arr):
+        if '//' not in arr[idxLine]:
+            output += arr[idxLine]
+        idxLine += 1
+
+    return output
 
 
-def ignoreindentation(arr):
-    # Menghapus indentasi pada array
+def splitSyntax(rawSyntax):
+    # Memisahkan string yang terpisah oleh spasi
+    processedSyntax = rawSyntax.split(" ")
 
-    # KAMUS
+    # Membuat List Separator (Terbuat dari Terminal)
+    listOfSeparators = [
+        # Ini Terminal berupa Kata Perintah di JS
+        'break',
+        'continue',
+        'return',
+        'const',
+        'var',
+        'let',
+        'switch',
+        'case',
+        'try'
+        'catch',
+        'finally',
+        'throw',
+        'default',
+        'delete',
+        'if',
+        'else',
+        'true',
+        'false',
+        'null',
+        'for',
+        'while',
+        'function',
 
-    # ALGORITMA
-    for i in range(len(arr)):
-        while len(arr[i]) > 0 and arr[i][0] == ' ':
-            arr[i] = arr[i][1:]
-            print(arr[i])
+        # Ini bagian simbol-simbol di JS
+        r'\;',
+        r'\(',
+        r'\)',
+        r'\{',
+        r'\}',
+        r'\[',
+        r'\]',
+        '\n',
 
-    return arr
+        # Ini bagian operator
+        r'\+',
+        r'\-',
+        r'\*',
+        r'\/',
+        r'\=',
+        r'\!',
+        r'\~',
+        r'\%',
+        r'\<',
+        r'\>',
+        r'\&',
+        r'\|',
+        r'\^',
+        r'\,',
+        r'\.'
+    ]
+
+    for separator in listOfSeparators:
+        tempSyntax = []
+
+        for s in processedSyntax:
+            tempSyntax += re.split(rf'({separator})', s)
+
+        processedSyntax = tempSyntax
+
+    while '' in processedSyntax:
+        processedSyntax.remove('')
+
+    return processedSyntax
 
 
 if __name__ == "__main__":
-    # Driver Bacafile
+    raw = bacaFile("inputAcc.js")
+    processed = splitSyntax(raw)
 
-    # KAMUS
-    # arraytextfile : array of string
-
-    # ALGORITMA
-    arraytextfile = bacafile("inputAcc.js")
-    ignoreindentation(arraytextfile)
-
-    print(arraytextfile)
+    for s in processed:
+        if s != '\n':
+            print(s)
---
+++ FA.py
@@ -0,0 +1,127 @@
+import BacaFile2
+
+
+listOfSyntaxWord = [
+    # Ini Terminal berupa Kata Perintah di JS
+    'break',
+    'continue',
+    'return',
+    'const',
+    'var',
+    'let',
+    'switch',
+    'case',
+    'try'
+    'catch',
+    'finally',
+    'throw',
+    'default',
+    'delete',
+    'if',
+    'else',
+    'true',
+    'false',
+    'null',
+    'for',
+    'while',
+    'function',
+
+    # Ini bagian simbol-simbol di JS
+    ';',
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    '\n',
+
+    # Ini bagian operator
+    '+',
+    '-',
+    '*',
+    '/',
+    '==',
+    '!',
+    '~',
+    '%',
+    '<',
+    '>',
+    '&',
+    '|',
+    '^',
+    ',',
+    '.'
+]
+
+
+def validateNonVar(processedSyntax):
+    global listOfSyntaxWord
+
+    for index in range(len(processedSyntax)):
+        if not isSyntaxWord(processedSyntax[index]):
+            if '0' <= processedSyntax[index][0] <= '9':
+                if numFA(processedSyntax[index]):
+                    processedSyntax[index] = "validNum"
+                else:
+                    processedSyntax[index] = "invalidNum"
+            else:
+                if varAndFunFA(processedSyntax[index]):
+                    processedSyntax[index] = "validName"
+                else:
+                    processedSyntax[index] = "invalidName"
+
+    return processedSyntax
+
+
+def varAndFunFA(string):
+    # FA untuk pengecekan nama variabel dan fungsi
+
+    # States
+    # q0 : Baca Huruf Pertama
+    # q1 : Final State (nama Var/Fun valid)
+    # q2 : Dead State
+
+    currentState = "q0"
+
+    for char in string:
+        if not ('a' <= char <= 'z' or 'A' <= char <= 'Z' or '0' <= char <= '9' or char == '_' or char == '$'):
+            currentState = "q2"
+            break
+
+        if currentState == "q0":
+            currentState = "q1"
+
+    return currentState == "q1"
+
+
+def numFA(string):
+    # FA untuk pengecekan angka
+    # Input sudah pasti tidak kosong
+
+    # States
+    # q0 : Final State, string yang dibaca semua berisi angka
+    # q1 : Dead State
+
+    currentState = "q0"
+
+    for char in string:
+        if not '0' <= char <= '9':
+            currentState = "q1"
+            break
+
+    return currentState == "q0"
+
+
+def isSyntaxWord(string):
+    global listOfSyntaxWord
+
+    return string in listOfSyntaxWord
+
+
+if __name__ == "__main__":
+    processedSyntax = BacaFile2.splitSyntax(BacaFile2.bacaFile("inputAcc.js"))
+
+    # processedSyntax = ['9---', 'An+jay', 'Woee', '$$$', '1000000', 'const']
+
+    print(validateNonVar(processedSyntax))
